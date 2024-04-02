d2f9c3ff38616db5324193e97614b61b8cc7ea06
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-04-02 19:58:38 +0300

    success test with robot

--- src/test_motor_and_radio/test_motor_and_radio.ino
+++ src/test_motor_and_radio/test_motor_and_radio.ino
@@ -1,27 +1,35 @@
-// https://3d-diy.ru/wiki/arduino-moduli/radio-modul-nrf24l01/
-// https://роботехника18.рф/nrf24l01-ардуино/
 
 #include <SPI.h>  // Подключаем библиотеку для работы с SPI-интерфейсом
 #include <nRF24L01.h> // Подключаем файл конфигурации из библиотеки RF24
 #include <RF24.h> // Подключаем библиотеку для работа для работы с модулем NRF24L01
-#define PIN_POT A0  // Номер пина Arduino, к которому подключен потенциометр
+#define PIN_LED 3  // Номер пина Arduino, к которому подключен светодиод
 #define PIN_CE  9  // Номер пина Arduino, к которому подключен вывод CE радиомодуля
 #define PIN_CSN 10 // Номер пина Arduino, к которому подключен вывод CSN радиомодуля
 RF24 radio(PIN_CE, PIN_CSN); // Создаём объект radio с указанием выводов CE и CSN
 
-int potValue[1]; // Создаём массив для передачи значений потенциометра
+int potValue[1]; // Создаём массив для приёма значений потенциометра
 
 void setup() {
+  Serial.begin(9600);
+
+  pinMode(A0, OUTPUT); 
+  pinMode(A1, OUTPUT); 
+  digitalWrite(A1,0);
+
   radio.begin();  // Инициализация модуля NRF24L01
   radio.setChannel(5); // Обмен данными будет вестись на пятом канале (2,405 ГГц)
   radio.setDataRate (RF24_250KBPS); // Скорость обмена данными 1 Мбит/сек
   radio.setPALevel(RF24_PA_LOW); // Выбираем высокую мощность передатчика (-6dBm)
-  radio.openWritingPipe(0x7878787878LL); // Открываем трубу с уникальным ID
-  Serial.begin(9600);
+  radio.openReadingPipe (1, 0x7878787878LL); // Открываем трубу ID передатчика
+  radio.startListening(); // Начинаем прослушивать открываемую трубу
 }
 
 void loop() {
-  potValue[0] = analogRead(PIN_POT)/4; // Считываем показания потенциометра
-  Serial.println(potValue[0]);
-  radio.write(potValue, 1); // Отправляем считанные показания по радиоканалу
+  if(radio.available()){ // Если в буфер приёмника поступили данные
+    radio.read(&potValue, sizeof(potValue)); // Читаем показания потенциометра
+    Serial.println(potValue[0]);
+    digitalWrite(A0, potValue[0]>200); // Регулируем яркость диода
+  }
 }
+
+
