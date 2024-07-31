3d82fc9cc587d79afec84f19c0a445859f74d351
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-07-31 22:13:29 +0300

    first test Shield - success

--- src/robot/robot.ino
+++ src/robot/robot.ino
@@ -8,25 +8,22 @@
 */
 
 #include "pins.h"
-#include "motor.h"
+// #include "motor.h"
 // #include "FlySky.h"
 #include "FlySky_uart.h"
 
-Motor motor_1;
-Motor motor_2;
+#include "BTS7960_PRO.h"
+BTS7960_PRO motors;
 
 #include "gy-25.h"
-GY25 gy25(9,2); // (TX,RX) - пины гироскопа
+GY25 gy25(12,8); // (TX,RX) - пины гироскопа
 // GY25_1 gy25;
 
 void setup() {
   // Serial.begin(9600);
   flysky.begin(Serial);
   // flysky.setup();
-  motor_1.setup(MOTOR_1_DIR,MOTOR_1_PWM);
-  motor_2.setup(MOTOR_2_DIR,MOTOR_2_PWM,-1.0);
-  motor_1.run();
-  motor_2.run();
+  motors.setup();
   gy25.setup();
 }
 
@@ -46,7 +43,7 @@ void loop() {
 void mainFlysky() {
   int x=0,y=0;
   x = flysky.readChannel(FLYSKY_JOYSTICK_RIGHT_X);
-  y = flysky.readChannel(FLYSKY_JOYSTICK_RIGHT_Y);
+  y = -flysky.readChannel(FLYSKY_JOYSTICK_RIGHT_Y);
   int rotation_new = flysky.readChannel(FLYSKY_JOYSTICK_LEFT_X);
   if (abs(rotation_new)>50) {
     if (t<millis()) {
@@ -57,8 +54,8 @@ void mainFlysky() {
 
   gy25.update();
   long int e = rotation - gy25.horizontal_angle;
-  if (abs(e)>5) x -= e*3.5;
+  if (abs(e)>5) x -= e*3;
 
-  motor_1.run(y-x);
-  motor_2.run(y+x);
+  motors.run(1,y-x);
+  motors.run(2,y+x);
 }
